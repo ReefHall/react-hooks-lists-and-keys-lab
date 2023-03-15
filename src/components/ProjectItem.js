dddd6ccad02b65b96818bb5728c68f9ab7108dfd
Shareif Hall <shareifhall@yahoo.com>
2023-03-15 01:15:09 -0400

    complete

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,15 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  const names = technologies.map(name => {
+   return <span key ={name}>{name}</span>
+  })
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {names}
       </div>
     </div>
   );
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ProjectItem from "./ProjectItem";
+import user from "../data/user";
 
 function ProjectList({ projects }) {
   console.log(projects);
+  const techs = user.projects.map(pro => {
+   return <ProjectItem key = {pro.id} name = {pro.name} about = {pro.about} technologies ={pro.technologies}/>
+  })
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{techs}</div>
     </div>
   );
 }
